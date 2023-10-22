66f919b0b462779d85526991d566fac81ae52a7c
2279244533 <131430121+2279244533@users.noreply.github.com>
2023-10-22 22:05:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,4 +19,3 @@
 # Modify default IP
 git clone https://github.com/CHN-beta/rkp-ipid package/rkp-ipid
 git clone https://github.com/Zxilly/UA2F package/UA2F
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
