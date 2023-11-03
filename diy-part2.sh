cd1d15a13586f00a5e468a901fccc9d20492db42
2279244533 <131430121+2279244533@users.noreply.github.com>
2023-11-03 14:45:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 #
 # Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
@@ -10,6 +9,13 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+
+# CONFIG_NETFILTER=y
+# CONFIG_NETFILTER_NETLINK=y
+# CONFIG_NETFILTER_NETLINK_GLUE_CT=y
+# CONFIG_NETFILTER_NETLINK_LOG=y
+# CONFIG_NF_CONNTRACK=y
+# CONFIG_NF_CT_NETLINK=y
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
